ed16aa544972199535e3e4ee0295d8981cf9cc48
KUSHARGAVARMA <kushavrm008@gmail.com>
2023-12-12 10:40:20 +0530

    initial

--- HTTP-SERVER-1/index.js
+++ HTTP-SERVER-1/index.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const app= express();
+app.use('express.json()');
 const port=3000
 app.get('/',(req,res)=>{
     res.send("Hello world");
 })
-
+app.post('/',(req,res)=>{
+    console.log(req.body);
+})
+// create a todo app that lets users store todos on the server
+// try to create a http sever from scratch in c
+// createa a http server in golang
 app.listen(port);
